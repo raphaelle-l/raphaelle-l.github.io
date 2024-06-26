063a811952cf9c4bd741182487d1ae86085891d0
raphaelle-l <raphaelle.lepere@gmail.com>
2024-06-26 14:39:33 -0600

    Update main.js

--- wa/wa10/main.js
+++ wa/wa10/main.js
@@ -4,7 +4,7 @@ var story = document.querySelector('.story');
 
 var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 var insertX = ['Ralphie','Chip'];
-var insertY = ['the soup kitchen','Disneyland','the White House'];
+var insertY = ['Farrand Field','CU Stadium'];
 var insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
 function randomValFrmArr(array) {
