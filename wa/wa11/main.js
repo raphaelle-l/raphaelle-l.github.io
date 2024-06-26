dcd4931fa603060349b6239e0f19b3dc8fc6c5b2
raphaelle-l <raphaelle.lepere@gmail.com>
2024-06-26 16:25:10 -0600

    Update main.js

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -8,11 +8,11 @@ const overlay = document.querySelector('.overlay');
 
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
+  'pic1.jpg' : 'pic 1',
+  'pic2.jpg' : 'pic 2',
+  'pic3.jpg' : 'pic 3',
+  'pic4.jpg' : 'pic 4',
+  'pic5.jpg' : 'pic 5'
 }
 
 /* Looping through images */
