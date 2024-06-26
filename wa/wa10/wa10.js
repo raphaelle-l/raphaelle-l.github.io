c1f093cc66a25e1f3548fc76ddfecd229cebc132
raphaelle-l <raphaelle.lepere@gmail.com>
2024-06-26 09:50:39 -0600

    Update wa10.js

--- wa/wa10/wa10.js
+++ wa/wa10/wa10.js
@@ -1,5 +1,3 @@
-// Step 1
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -9,36 +7,40 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'Once upon a time in a whimsical village, :insertx:, a mischievous cat with a penchant for shiny treasures, stumbled upon a dazzling diamond ring. Unaware it belonged to the Queen, :insertx: proudly flaunted it around town, causing quite a stir. After a merry chase, :insertx: returned the ring, earning himself a reputation as the town's charming diamond cat, beloved by all for :insertx:'s playful antics.'
-const insertX = ['Chip','Ralphie'];
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = [
+"Willy the Goblin",
+"Big Daddy",
+"Father Christmas"];
+
+let insertY = [
+"the soup kitchen",
+"Disneyland",
+"the White House"];
+
+let insertZ = [
+"spontaneously combusted",
+"melted into a puddle on the sidewalk",
+"turned into a slug and crawled away"];
 
 
-
-// STEP 3
 randomize.addEventListener('click', result);
 
 function result() {
-  let newStory = storyText;
-const xItem = randomValueFromArray(insertX);
-const yItem = randomValueFromArray(insertY);
-const zItem = randomValueFromArray(insertZ);
-  
-  newStory = newStory.replaceAll(':insertx:', xItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob', name);
-    
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + ' stone';
-    const temperature =  Math.round(((94 - 32) * 5/9)) + ' centigrade';
-    newStory = newStory.replace('300 pounds', weight);
-    newStory = newStory.replace('94 fahrenheit', temperature);
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
   }
 
-  story.textContent = newStory;
+  story.textContent = ;
   story.style.visibility = 'visible';
 }
