658638429a9f9b5f5890a87f1d7a8c87374ab614
raphaelle-l <raphaelle.lepere@gmail.com>
2024-06-30 16:36:51 -0600

    Update ica12.js

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -1,7 +1,7 @@
 const newQuoteButton = document.querySelector('#js-new-quote');
 newQuoteButton.addEventListener('click', getQuote);
 
-const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestio';
 
 async function getQuote() {
   try {
