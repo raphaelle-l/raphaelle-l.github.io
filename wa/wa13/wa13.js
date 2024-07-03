9383c67588ea3427feb5aac1273c84e0fc087059
raphaelle-l <raphaelle.lepere@gmail.com>
2024-07-03 15:03:15 -0600

    Create wa13.js

---
+++ wa/wa13/wa13.js
@@ -0,0 +1,64 @@
+{
+  "employees": [
+    {
+      "employeeID": 1,
+      "name": "John Smith",
+      "salary": 50000,
+      "department": "Accounting"
+    },
+    {
+      "employeeID": 2,
+      "name": "Jane Doe",
+      "salary": 60000,
+      "department": "Human Resources"
+    },
+    {
+      "employeeID": 3,
+      "name": "Mike Johnson",
+      "salary": 40000,
+      "department": "IT"
+    },
+    {
+      "employeeID": 4,
+      "name": "Mary Williams",
+      "salary": 45000,
+      "department": "Marketing"
+    },
+    {
+      "employeeID": 5,
+      "name": "Steve Brown",
+      "salary": 55000,
+      "department": "Sales"
+    },
+    {
+      "employeeID": 6,
+      "name": "Barbara Jones",
+      "salary": 75000,
+      "department": "Accounting"
+    },
+    {
+      "employeeID": 7,
+      "name": "Susan Adams",
+      "salary": 80000,
+      "department": "Human Resources"
+    },
+    {
+      "employeeID": 8,
+      "name": "Bill Wilson",
+      "salary": 65000,
+      "department": "IT"
+    },
+    {
+      "employeeID": 9,
+      "name": "Karen Spencer",
+      "salary": 95000,
+      "department": "Marketing"
+    },
+    {
+      "employeeID": 10,
+      "name": "Mike Parker",
+      "salary": 100000,
+      "department": "Sales"
+    }
+  ]
+}
