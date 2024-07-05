4daeada36e677874131ea67ecfee034e3c85b278
raphaelle-l <raphaelle.lepere@gmail.com>
2024-07-05 15:02:17 -0600

    Update jsproject.js

--- js project/jsproject.js
+++ js project/jsproject.js
@@ -2,6 +2,10 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
+
+  function displayText() {
+  var text = document.getElementById("textField");
+  text.style.display = "block";
+}
