b94bb4dd49b169abb91c9be57eca83f00aaa1cc5
raphaelle-l <raphaelle.lepere@gmail.com>
2024-11-18 14:41:01 -0700

    wa12

---
+++ wa/wa12/wa12.js
@@ -0,0 +1,27 @@
+
+let myData = {};
+let num = Math.floor(Math.random() * 100);
+let url = `https://corsproxy.io/?https://xkcd.com/${num.toString()}/info.0.json`
+console.log(url);
+
+function fetchData(){
+fetch(`https://corsproxy.io/?https://xkcd.com/${num.toString()}/info.0.json`)
+    .then(res =>{
+        if(res.ok){
+            return res.json();
+
+        }else{
+            console.log(res);
+        }
+    }).then(res => {
+        myData = res;
+        console.log(myData);
+        document.getElementById("fact").innerHTML = myData.fact;
+    })
+    .catch(error => {console.log(error)})
+}
+fetchData();
+
+document.getElementById("generate").addEventListener("click", e=> {fetchData();})
+
+    console.log(json);
