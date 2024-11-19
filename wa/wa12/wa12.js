4101f132315557abe02c27b23f85423e6599dd38
raphaelle-l <raphaelle.lepere@gmail.com>
2024-11-18 19:15:32 -0700

    wa12a

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -8,7 +8,7 @@ function fetchData(){
 fetch(`https://corsproxy.io/?https://xkcd.com/${num.toString()}/info.0.json`)
     .then(res =>{
         if(res.ok){
-            return res.json();
+            return res.json(); 
 
         }else{
             console.log(res);
@@ -25,3 +25,34 @@ fetchData();
 document.getElementById("generate").addEventListener("click", e=> {fetchData();})
 
     console.log(json);
+
+    const btn = document.querySelector('button');
+    const comic = document.querySelector('p');
+    const comicTitle = document.querySelector('h1');
+    const comicImage = document.querySelector('img');
+
+    async function getComic()
+    let rand = Math.floor(Math.random() * (3000)) + 1;
+    let num = rand.toString();
+    const apiUrl = `https://corsproxy.io/?https://xkcd.com/${num}/info.0.json`;
+    const response = await fetch(apiUrl);
+    if(!response.ok){
+        throw Error(response.statusText);
+    }
+    const json = await response.json();
+    console.log(json);
+
+    const title = json.safe_title;
+    const year = json.year;
+    const img = json.img;
+    const alt = json.alt;
+    comicTitle.innerText = title;
+    comicImage.setAttribute('src', img);
+    comicImage.setAttribute('alt', alt);
+    comic.innerText = year;
+
+    getComic();
+    btn.addEventListener('click', () => {
+        console.log('clicked')
+        getComic();
+    })
